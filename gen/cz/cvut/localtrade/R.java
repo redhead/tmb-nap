00c451923022bee961fe4c9651672f4db9b11af6
medy75 <jarin.md75@gmail.com>
2012-11-02 00:29:16 +0100

    Fixed some string issues

--- gen/cz/cvut/localtrade/R.java
+++ gen/cz/cvut/localtrade/R.java
@@ -80,27 +80,28 @@ public final class R {
         public static final int app_description=0x7f050008;
         public static final int app_name=0x7f050000;
         public static final int buy_button=0x7f050005;
+        public static final int filter=0x7f05000b;
         public static final int hello_world=0x7f050001;
-        public static final int itemDistance=0x7f050018;
-        public static final int itemPrice=0x7f050016;
-        public static final int itemRealPrice=0x7f050017;
-        public static final int itemRealState=0x7f050015;
-        public static final int itemState=0x7f050014;
-        public static final int itemTitle=0x7f050013;
-        public static final int login=0x7f05000b;
-        public static final int logout=0x7f05000e;
+        public static final int itemDistance=0x7f050019;
+        public static final int itemPrice=0x7f050017;
+        public static final int itemRealPrice=0x7f050018;
+        public static final int itemRealState=0x7f050016;
+        public static final int itemState=0x7f050015;
+        public static final int itemTitle=0x7f050014;
+        public static final int login=0x7f05000c;
+        public static final int logout=0x7f05000f;
         public static final int menu_settings=0x7f050002;
-        public static final int myItems=0x7f050010;
-        public static final int password=0x7f05000d;
-        public static final int photo=0x7f050012;
-        public static final int search=0x7f050011;
+        public static final int myItems=0x7f050011;
+        public static final int password=0x7f05000e;
+        public static final int photo=0x7f050013;
+        public static final int search=0x7f050012;
         public static final int sell_button=0x7f050006;
-        public static final int showList=0x7f05000f;
+        public static final int showList=0x7f050010;
         public static final int showMap_button=0x7f050007;
         public static final int splashMap=0x7f050009;
         public static final int test=0x7f05000a;
         public static final int title_activity_splash=0x7f050003;
-        public static final int username=0x7f05000c;
+        public static final int username=0x7f05000d;
     }
     public static final class style {
         /**  Any customizations for your app running on pre-3.0 devices here 
