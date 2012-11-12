7a3bb3280b3bc54c45959c19a1ba44c613eb9e63
redhead <redhead@email.cz>
2012-11-13 00:18:28 +0100

    removed unused imports

--- src/cz/cvut/localtrade/ConversationActivity.java
+++ src/cz/cvut/localtrade/ConversationActivity.java
@@ -1,7 +1,6 @@
 package cz.cvut.localtrade;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
--- src/cz/cvut/localtrade/SearchedItemsActivity.java
+++ src/cz/cvut/localtrade/SearchedItemsActivity.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
